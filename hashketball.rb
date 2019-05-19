a295a421f79409a8ee67b387906c26f5336a8b10
Danielle Mizrachi <dmiz@brandeis.edu>
2019-05-19 10:03:37 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -116,7 +116,7 @@ def game_hash
   }
 end
 
-def num_points_scored(player_n)
+def num_points_scored(player)
    game_hash.each do |location, team_data|
     team_data.each do |attribute, data|
       if data.include?(player)
